bb5d094ca4e5c7224391ad14f5ae1842bfdce925
Mobin <mobin04dev@gmail.com>
2024-02-23 20:35:53 +0330

    create new app(cart)

--- shop/signals.py
+++ shop/signals.py
@@ -2,7 +2,8 @@ from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from .models import Product
 
+
 @receiver(pre_save, sender=Product)
 def calculate_new_price(sender, instance, **kwargs):
-	percent_price = (instance.price / 100) * instance.off
-	instance.new_price = instance.price - percent_price
+    percent_price = (instance.price / 100) * instance.off
+    instance.new_price = instance.price - percent_price
